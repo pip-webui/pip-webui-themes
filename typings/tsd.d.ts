74e34018b696f1cbb3d8a874d489d6dad9becd2c
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-31 22:06:22 -0500

    Reimplemented layouts

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -4,8 +4,4 @@
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="angular-material/angular-material.d.ts" />
 /// <reference path="core-js/core-js.d.ts" />
-/// <reference path="angularjs/angular-resource.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="assertion-error/assertion-error.d.ts" />
-/// <reference path="chai/chai.d.ts" />
+
