450431ea3c7d8d15d884e6cc6e22e29e9e6cc47e
Anastas Fonotov <anastasf@gmail.com>
2016-11-03 11:42:52 +0200

    update

--- dist/pip-webui-themes.d.ts
+++ dist/pip-webui-themes.d.ts
@@ -1,5 +1,12 @@
 declare module pip.themes {
 
+function configureBootBarnCoolTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureBootBarnMonochromeTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureBootBarnWarmTheme($mdThemingProvider: any): void;
+
+
 
 export let ThemeRootVar: string;
 export let ThemeChangedEvent: string;
@@ -11,13 +18,6 @@ export interface IThemeProvider extends IThemeService, ng.IServiceProvider {
     setRootVar: boolean;
     persist: boolean;
 }
-
-function configureBootBarnCoolTheme($mdThemingProvider: ng.material.IThemingProvider): void;
-
-function configureBootBarnMonochromeTheme($mdThemingProvider: ng.material.IThemingProvider): void;
-
-function configureBootBarnWarmTheme($mdThemingProvider: any): void;
-
 
 function configureDefaultAmberTheme($mdThemingProvider: ng.material.IThemingProvider): void;
 
