56ea65b2ed13bc1044b9bd5a0fd44e9745289450
Anastas Fonotov <anastasf@gmail.com>
2017-07-25 13:00:13 +0300

    'rebuild'

--- dist/pip-webui-themes.d.ts
+++ dist/pip-webui-themes.d.ts
@@ -5,6 +5,15 @@ declare module pip.themes {
 
 
 
+
+
+
+
+
+
+
+
+
 export interface IThemeService {
     theme: string;
     use(language: string): string;
@@ -17,15 +26,6 @@ export interface IThemeProvider extends IThemeService, ng.IServiceProvider {
 export let ThemeRootVar: string;
 export let ThemeChangedEvent: string;
 export let ThemeResetPage: string;
-
-
-
-
-
-
-
-
-
 
 }
 
